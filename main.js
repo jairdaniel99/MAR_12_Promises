c5da64695ec1f9f66a127a634e902b96b978039a
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-03-12 18:56:04 -0600

    using promise instead

--- main.js
+++ main.js
@@ -29,3 +29,34 @@ getUser(1, function (user) {
 });
 
 console.log("After");
+
+// what is a promise?
+// a promise is an object that represents the eventual completion or failure of an asynchronous operation.
+// a promise can be in three states: pending, fullfilled/resolved, or rejected/error;
+// a new promise starts off in the pending state. It can then transition to the fulfilled state if the operation is successful, or the rejected state if failed
+
+function getUserAsync(id) {
+  //returns a primise object
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      // once we have our result from the database, we provide it to the callback function
+      let user = { id: id, githubUsername: "ShareghYusefi" };
+      if (id !== 1) reject(new Error("Usewr not found"));
+      callback(user);
+    }, 3000);
+  });
+}
+
+console.log("Before");
+
+// the .then method is used to handle the result of a succesful promise
+let promiseObject = getUserAsync(1);
+console.log(promiseObject);
+promiseObject
+  .then((result) => console.log(result))
+  .catch((error) => {
+    console.log("Error: ", error);
+  });
+
+console.log("After");
